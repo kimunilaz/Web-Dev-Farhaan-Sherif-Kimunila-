ef874898060be7c2986a0c9492fc188a6254f3ee
Sherif <sumailasherif773@gmail.com>
2026-08-26 22:36:42 +0400

    initalised database connection

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/config/db.php';   // we add our database configuration file here so we can connect to our database.
+require_once __DIR__ . '/includes/auth.php'; //we add our authentication file here to handle user authentication and access
+
+$stmt = $pdo->query('SELECT id, hero_name, real_name, short_bio, image_url, team FROM heroes ORDER BY hero_name ASC');
+$heroes = $stmt->fetchAll();
+

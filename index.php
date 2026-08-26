eff01f94476775845c6eba2bd88e41a2845c4ba5
Sherif <sumailasherif773@gmail.com>
2026-08-26 22:40:37 +0400

    Arranging hero name details in alphabetic order

--- index.php
+++ index.php
@@ -4,4 +4,6 @@ require_once __DIR__ . '/includes/auth.php'; //we add our authentication file he
 
 $stmt = $pdo->query('SELECT id, hero_name, real_name, short_bio, image_url, team FROM heroes ORDER BY hero_name ASC');
 $heroes = $stmt->fetchAll();
-
+$page_title = 'All Heroes';
+require __DIR__ . '/includes/header.php';
+?>
